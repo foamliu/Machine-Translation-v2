96e24b223537b7b40b2fbff622e5de486b85d73c
Yang Liu <foamliu@yeah.net>
2018-07-10 21:01:18 +0800

    update

--- model.py
+++ model.py
@@ -10,12 +10,12 @@ from config import hidden_size, vocab_size_en, vocab_size_zh, embedding_size, ma
 def build_model():
     en_input = Input(shape=(max_token_length_en,), dtype='int32')
     x = Embedding(input_dim=vocab_size_en, output_dim=embedding_size)(en_input)
-    x = LSTM(hidden_size, return_sequence=False)(x)
+    x = LSTM(hidden_size, return_sequences=False)(x)
     en_embedding = Dense(embedding_size)(x)
 
     zh_input = Input(shape=(max_token_length_zh,), dtype='int32')
     x = Embedding(input_dim=vocab_size_zh, output_dim=embedding_size)(zh_input)
-    x = LSTM(hidden_size, return_sequence=True)(x)
+    x = LSTM(hidden_size, return_sequences=True)(x)
     zh_embedding = TimeDistributed(Dense(embedding_size))(x)
 
     x = [en_embedding, zh_embedding]
