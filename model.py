2c9b70fd35c8672a453a4d3e345b93d667bbda9b
Yang Liu <foamliu@yeah.net>
2018-07-12 18:14:15 +0800

    update

--- model.py
+++ model.py
@@ -1,22 +1,20 @@
 import keras.backend as K
 import tensorflow as tf
-from keras.layers import Input, Dense, LSTM, Embedding, TimeDistributed, Concatenate, Bidirectional, RepeatVector
+from keras.layers import Input, Dense, LSTM, TimeDistributed, Concatenate, Bidirectional, RepeatVector
 from keras.models import Model
 from keras.utils import plot_model
 
-from config import hidden_size, vocab_size_en, vocab_size_zh, embedding_size, max_token_length_en, max_token_length_zh
+from config import hidden_size, vocab_size_zh, embedding_size, max_token_length_en, max_token_length_zh
 
 
 def build_model():
-    en_input = Input(shape=(max_token_length_en,), dtype='int32')
-    x = Embedding(input_dim=vocab_size_en, output_dim=embedding_size)(en_input)
-    x = LSTM(hidden_size, return_sequences=False)(x)
+    en_input = Input(shape=(max_token_length_en, embedding_size), dtype='float32')
+    x = LSTM(hidden_size, return_sequences=False)(en_input)
     x = Dense(embedding_size)(x)
     en_embedding = RepeatVector(1)(x)
 
-    zh_input = Input(shape=(max_token_length_zh,), dtype='int32')
-    x = Embedding(input_dim=vocab_size_zh, output_dim=embedding_size)(zh_input)
-    x = LSTM(hidden_size, return_sequences=True)(x)
+    zh_input = Input(shape=(max_token_length_zh, embedding_size), dtype='float32')
+    x = LSTM(hidden_size, return_sequences=True)(zh_input)
     zh_embedding = TimeDistributed(Dense(embedding_size))(x)
 
     x = [en_embedding, zh_embedding]
