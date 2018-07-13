62b0d69a337ca5555f54ef22dc834676e3163a42
Yang Liu <foamliu@yeah.net>
2018-07-13 17:26:13 +0800

    update

--- model.py
+++ model.py
@@ -7,6 +7,27 @@ from keras.utils import plot_model
 from config import n_s, n_a, vocab_size_zh, embedding_size, Tx, Ty
 
 
+def softmax(x, axis=1):
+    """Softmax activation function.
+    # Arguments
+        x : Tensor.
+        axis: Integer, axis along which the softmax normalization is applied.
+    # Returns
+        Tensor, output of softmax transformation.
+    # Raises
+        ValueError: In case `dim(x) == 1`.
+    """
+    ndim = K.ndim(x)
+    if ndim == 2:
+        return K.softmax(x)
+    elif ndim > 2:
+        e = K.exp(x - K.max(x, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
+
+
 def one_step_attention(a, s_prev):
     print('a.shape: ' + str(a.shape))
     s_prev = RepeatVector(Tx)(s_prev)
@@ -14,7 +35,7 @@ def one_step_attention(a, s_prev):
     print('concat.shape: ' + str(concat.shape))
     e = Dense(10, activation="tanh")(concat)
     energies = Dense(1, activation="relu")(e)
-    alphas = Activation('softmax')(energies)
+    alphas = Activation(softmax)(energies)
     print('alphas.shape: ' + str(alphas.shape))
     context = Dot(axes=1)([alphas, a])
     print('context.shape: ' + str(context.shape))
