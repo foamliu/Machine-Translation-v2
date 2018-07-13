9d26e33b47b8d97d95cdaa4131aaa78e2ab55659
Yang Liu <foamliu@yeah.net>
2018-07-13 17:57:17 +0800

    update

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import keras.backend as K
 import tensorflow as tf
-from keras.layers import Input, Dense, GRU, Concatenate, Bidirectional, RepeatVector, Activation, Dot
+from keras.layers import Input, Dense, LSTM, Concatenate, Bidirectional, RepeatVector, Activation, Dot
 from keras.models import Model
 from keras.utils import plot_model
 
@@ -51,12 +51,12 @@ def build_model():
 
     outputs = []
 
-    a = Bidirectional(GRU(n_a, return_sequences=True, implementation=2))(X)
+    a = Bidirectional(LSTM(n_a, return_sequences=True))(X)
     print('a.shape: ' + str(a.shape))
 
     for t in range(Ty):
         context = one_step_attention(a, s)
-        s, _, c = GRU(n_s, return_state=True, implementation=2)(context, initial_state=[s, c])
+        s, _, c = LSTM(n_s, return_state=True)(context, initial_state=[s, c])
         out = Dense(vocab_size_zh, activation='softmax')(s)
         outputs.append(out)
 
