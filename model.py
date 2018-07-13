8565b3fcad8c63006f4542fcc93ef974913d5532
Yang Liu <foamliu@yeah.net>
2018-07-13 17:20:57 +0800

    update

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import keras.backend as K
 import tensorflow as tf
-from keras.layers import Input, Dense, LSTM, Concatenate, Bidirectional, RepeatVector, Activation, \
-    Dot
+from keras.layers import Input, Dense, GRU, Concatenate, Bidirectional, RepeatVector, Activation, Dot
 from keras.models import Model
 from keras.utils import plot_model
 
@@ -9,12 +8,16 @@ from config import n_s, n_a, vocab_size_zh, embedding_size, Tx, Ty
 
 
 def one_step_attention(a, s_prev):
+    print('a.shape: ' + str(a.shape))
     s_prev = RepeatVector(Tx)(s_prev)
     concat = Concatenate(axis=-1)([a, s_prev])
+    print('concat.shape: ' + str(concat.shape))
     e = Dense(10, activation="tanh")(concat)
     energies = Dense(1, activation="relu")(e)
     alphas = Activation('softmax')(energies)
+    print('alphas.shape: ' + str(alphas.shape))
     context = Dot(axes=1)([alphas, a])
+    print('context.shape: ' + str(context.shape))
     return context
 
 
@@ -27,12 +30,12 @@ def build_model():
 
     outputs = []
 
-    a = Bidirectional(LSTM(n_a, return_sequences=True))(X)
+    a = Bidirectional(GRU(n_a, return_sequences=True, implementation=2))(X)
     print('a.shape: ' + str(a.shape))
 
     for t in range(Ty):
         context = one_step_attention(a, s)
-        s, _, c = LSTM(n_s, return_state=True)(context, initial_state=[s, c])
+        s, _, c = GRU(n_s, return_state=True, implementation=2)(context, initial_state=[s, c])
         out = Dense(vocab_size_zh, activation='softmax')(s)
         outputs.append(out)
 
