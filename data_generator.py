56ac2426369ef34be60f4cdc34600f386c304316
Yang Liu <foamliu@yeah.net>
2018-07-13 11:07:15 +0800

    update

--- data_generator.py
+++ data_generator.py
@@ -7,7 +7,7 @@ from gensim.models import KeyedVectors
 from keras.utils import Sequence
 
 from config import batch_size, vocab_size_zh, max_token_length_en, max_token_length_zh, unknown_word, unknown_embedding, \
-    embedding_size
+    embedding_size, n_s
 
 print('loading fasttext en word embedding')
 word_vectors_en = KeyedVectors.load_word2vec_format('data/wiki.en.vec')
@@ -18,6 +18,7 @@ word_vectors_zh = KeyedVectors.load_word2vec_format('data/sgns.merge.char')
 class DataGenSequence(Sequence):
     def __init__(self, usage):
         self.usage = usage
+
         print('loading {} samples'.format(usage))
         if usage == 'train':
             samples_path = 'data/samples_train.p'
@@ -35,29 +36,28 @@ class DataGenSequence(Sequence):
         i = idx * batch_size
 
         length = min(batch_size, (len(self.samples) - i))
-        batch_y = np.empty((length, vocab_size_zh), dtype=np.int32)
 
-        batch_text_embedding_en = np.zeros((length, max_token_length_en, embedding_size), np.float32)
-        batch_text_embedding_zh = np.zeros((length, max_token_length_zh, embedding_size), np.float32)
+        batch_x = np.zeros((length, max_token_length_en, embedding_size), np.float32)
+        batch_y = np.zeros((length, max_token_length_zh), np.int32)
+
+        s0 = np.zeros((length, n_s))
+        c0 = np.zeros((length, n_s))
 
         for i_batch in range(length):
             sample = self.samples[i + i_batch]
 
-            for idx, word in enumerate(['input_en']):
+            for idx, word in enumerate(['input']):
                 if word == unknown_word:
-                    batch_text_embedding_en[i_batch, idx] = unknown_embedding
+                    batch_x[i_batch, idx] = unknown_embedding
                 else:
-                    batch_text_embedding_en[i_batch, idx] = word_vectors_en[word]
+                    batch_x[i_batch, idx] = word_vectors_en[word]
 
-            for idx, word in enumerate(sample['input_zh']):
-                if word == unknown_word:
-                    batch_text_embedding_zh[i_batch, idx] = unknown_embedding
-                else:
-                    batch_text_embedding_zh[i_batch, idx] = word_vectors_zh[word]
+            for i, idx in sample['output']:
+                batch_y[i_batch, i] = idx
 
             batch_y[i_batch] = keras.utils.to_categorical(sample['output'], vocab_size_zh)
 
-        return [batch_text_embedding_en, batch_text_embedding_zh], batch_y
+        return [batch_x, s0, c0], batch_y
 
     def on_epoch_end(self):
         np.random.shuffle(self.samples)
