8133382f99efb92f0ef07fe384ab25de63be380e
Yang Liu <foamliu@yeah.net>
2018-07-20 14:11:14 +0800

    OOM, reduce batch size

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import argparse
-
+import tensorflow as tf
 import keras
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
-from config import patience, epochs, num_train_samples, num_valid_samples, batch_size
+from config import patience, epochs, num_train_samples, num_valid_samples, batch_size, Ty
 from data_generator import train_gen, valid_gen
 from model import build_model
 from utils import ensure_folder
@@ -41,7 +41,8 @@ if __name__ == '__main__':
         new_model.load_weights(pretrained_path)
 
     adam = keras.optimizers.Adam(lr=0.002, beta_1=0.9, beta_2=0.999, clipvalue=5.)
-    new_model.compile(optimizer=adam, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    decoder_target = tf.placeholder(dtype='int32', shape=(None, Ty))
+    new_model.compile(optimizer=adam, loss='sparse_categorical_crossentropy', metrics=['accuracy'], target_tensors=[decoder_target])
 
     print(new_model.summary())
 
