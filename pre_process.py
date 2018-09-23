988f96460c3a0805094e4bb1c87aba8401fd147b
Yang Liu <foamliu@microsoft.com>
2018-09-23 16:17:07 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -1,7 +1,5 @@
-import os
 import pickle
 import xml.etree.ElementTree
-import zipfile
 from collections import Counter
 
 import jieba
@@ -9,18 +7,8 @@ import nltk
 from gensim.models import KeyedVectors
 from tqdm import tqdm
 
-from config import start_word, stop_word, unknown_word, Tx, Ty, vocab_size_zh
-from config import train_folder, valid_folder, test_a_folder, test_b_folder
-from config import train_translation_folder, train_translation_zh_filename, train_translation_en_filename
-from config import valid_translation_folder, valid_translation_zh_filename, valid_translation_en_filename
+from config import *
 from utils import ensure_folder
-
-
-def extract(folder):
-    filename = '{}.zip'.format(folder)
-    print('Extracting {}...'.format(filename))
-    with zipfile.ZipFile(filename, 'r') as zip_ref:
-        zip_ref.extractall('data')
 
 
 def build_train_vocab_zh():
@@ -129,18 +117,6 @@ def build_samples():
 
 if __name__ == '__main__':
     ensure_folder('data')
-
-    if not os.path.isdir(train_folder):
-        extract(train_folder)
-
-    if not os.path.isdir(valid_folder):
-        extract(valid_folder)
-
-    if not os.path.isdir(test_a_folder):
-        extract(test_a_folder)
-
-    if not os.path.isdir(test_b_folder):
-        extract(test_b_folder)
 
     if not os.path.isfile('data/vocab_train_zh.p'):
         build_train_vocab_zh()
