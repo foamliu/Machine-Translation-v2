2971457e544f759f69c6f75247ed20e56a95b70e
Yang Liu <foamliu@yeah.net>
2018-09-25 20:01:14 +0800

    update

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torch import nn
 from torch import optim
+from torch.autograd import Variable
 
 from config import *
 from data_generator import TranslationDataset
@@ -61,7 +62,7 @@ def train(train_loader, encoder, decoder, encoder_optimizer, decoder_optimizer, 
 
         encoder_outputs = torch.zeros(max_len, encoder.hidden_size, device=device)
 
-        loss = torch.Variable(0, requires_grad=True)
+        loss = Variable(0, requires_grad=True)
 
         for ei in range(input_length):
             encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
