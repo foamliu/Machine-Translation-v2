9c20017b228b6ad9e34ac9e8cb59d9a84538b340
Yang Liu <foamliu@yeah.net>
2018-09-26 17:09:13 +0800

    update

--- train.py
+++ train.py
@@ -176,7 +176,7 @@ def validate(val_loader, encoder, decoder, criterion):
         assert len(references) == len(hypotheses)
 
     # Calculate BLEU-4 scores
-    bleu4 = corpus_bleu(references, hypotheses, emulate_multibleu=True)
+    bleu4 = corpus_bleu(references, hypotheses)
 
     print(
         '\n * LOSS - {loss.avg:.3f}, BLEU-4 - {bleu}\n'.format(
