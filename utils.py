a293c44bdce6da4bfab0a22680141f3273419d60
Yang Liu <foamliu@yeah.net>
2018-09-28 11:04:12 +0800

    update

--- utils.py
+++ utils.py
@@ -7,6 +7,11 @@ from torch.autograd import Variable
 from config import *
 
 
+# Since we are dealing with batches of padded sequences, we cannot simply consider all elements of
+# the tensor when calculating loss. We define maskNLLLoss to calculate our loss based on our
+# decoder’s output tensor, the target tensor, and a binary mask tensor describing the padding of the
+# target tensor. This loss function calculates the average negative log likelihood of the elements that
+# correspond to a 1 in the mask tensor.
 def maskNLLLoss(inp, target, mask):
     nTotal = mask.sum()
     crossEntropy = -torch.log(torch.gather(input=inp, dim=1, index=target.view(-1, 1)))
