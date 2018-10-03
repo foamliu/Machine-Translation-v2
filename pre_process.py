689b45091ad1b631ab131aff916713a86689620a
Yang Liu <foamliu@microsoft.com>
2018-10-03 23:05:11 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -22,14 +22,14 @@ def build_wordmap_zh():
     word_freq = Counter()
 
     for sentence in tqdm(sentences):
-        seg_list = jieba.cut(sentence)
+        seg_list = jieba.cut(sentence.strip())
         # Update word frequency
         word_freq.update(list(seg_list))
 
     # Create word map
     # words = [w for w in word_freq.keys() if word_freq[w] > min_word_freq]
     words = word_freq.most_common(output_lang_vocab_size - 4)
-    word_map = {k: v + 4 for v, k in enumerate(words)}
+    word_map = {k[0]: v + 4 for v, k in enumerate(words)}
     word_map['<pad>'] = 0
     word_map['<start>'] = 1
     word_map['<end>'] = 2
@@ -58,7 +58,7 @@ def build_wordmap_en():
     # Create word map
     # words = [w for w in word_freq.keys() if word_freq[w] > min_word_freq]
     words = word_freq.most_common(input_lang_vocab_size - 4)
-    word_map = {k: v + 4 for v, k in enumerate(words)}
+    word_map = {k[0]: v + 4 for v, k in enumerate(words)}
     word_map['<pad>'] = 0
     word_map['<start>'] = 1
     word_map['<end>'] = 2
